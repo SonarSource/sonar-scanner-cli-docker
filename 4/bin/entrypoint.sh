87fa55ab7fa902f83550ad6f6d0c936558309d74
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2019-09-30 10:43:00 +0200

    [#8] SonarQube connection properties can be provided by env variables

--- 4/bin/entrypoint.sh
+++ 4/bin/entrypoint.sh
@@ -2,5 +2,17 @@
 
 set -euo pipefail
 
+declare -a args
+
+add_env_var_as_env_prop() {
+  if [ ! -z "$1" ]; then
+    args+=("-D$2=$1")
+  fi
+}
+
+add_env_var_as_env_prop "${SONAR_LOGIN:-}" "sonar.login"
+add_env_var_as_env_prop "${SONAR_PASSWORD:-}" "sonar.password"
+
 export SONAR_USER_HOME="$PWD/.sonar"
-sonar-scanner
+
+sonar-scanner "${args[@]}"
