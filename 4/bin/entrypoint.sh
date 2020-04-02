ae66510f2563b77a0dddbf7ba10a82d54d883f28
Adam Jabłoński <5859097+dzablo@users.noreply.github.com>
2020-04-02 15:59:53 +0200

    Update entrypoint.sh

--- 4/bin/entrypoint.sh
+++ 4/bin/entrypoint.sh
@@ -6,7 +6,7 @@ for ENV_NAME in $(compgen -e); do
     SONAR_CHECK=${ENV_NAME%%_*}
     if [ "$SONAR_CHECK" == "SONAR" ]; then
       NAME=${ENV_NAME#*_}
-      NAME=`echo $NAME | sed -e 's/\(.*\)/\L\1/' | sed -e 's/\(_[a-z]\)/\U\1/' | sed -e 's/_//'`
+      NAME=`echo $NAME | sed -e 's/__/./'` | sed -e 's/\(.*\)/\L\1/' | sed -e 's/\(_[a-z]\)/\U\1/' | sed -e 's/_//'`
       args+=("-Dsonar.$NAME=${!ENV_NAME}")
     fi
 
