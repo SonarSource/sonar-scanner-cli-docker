c68605f34030f5bc3c9c42014db56bcdb54d4b79
Michal Duda <michal.duda@sonarsource.com>
2020-05-15 13:47:49 +0200

    SQSCANNER-71 passing parameters fix and DockerHub consistency guidelines

--- 4/bin/entrypoint.sh
+++ 4/bin/entrypoint.sh
@@ -10,11 +10,6 @@ add_env_var_as_env_prop() {
   fi
 }
 
-add_env_var_as_env_prop "${SONAR_LOGIN:-}" "sonar.login"
-add_env_var_as_env_prop "${SONAR_PASSWORD:-}" "sonar.password"
-add_env_var_as_env_prop "${SONAR_USER_HOME:-}" "sonar.userHome"
-add_env_var_as_env_prop "${SONAR_PROJECT_BASE_DIR:-}" "sonar.projectBaseDir"
-
 PROJECT_BASE_DIR="$PWD"
 if [ "${SONAR_PROJECT_BASE_DIR:-}" ]; then
   PROJECT_BASE_DIR="${SONAR_PROJECT_BASE_DIR}"
@@ -22,8 +17,24 @@ fi
 
 export SONAR_USER_HOME="$PROJECT_BASE_DIR/.sonar"
 
+# if nothing is passed, assume we want to run sonar-scanner
+if [ "$#" == 0 ]; then
+  set -- sonar-scanner
+fi
+
+# if first arg looks like a flag, assume we want to run sonar-scanner with flags
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-  set -- sonar-scanner "${args[@]}"
+  set -- sonar-scanner "$@"
+fi
+
+if [[ "$1" = 'sonar-scanner' ]]; then
+  add_env_var_as_env_prop "${SONAR_LOGIN:-}" "sonar.login"
+  add_env_var_as_env_prop "${SONAR_PASSWORD:-}" "sonar.password"
+  add_env_var_as_env_prop "${SONAR_USER_HOME:-}" "sonar.userHome"
+  add_env_var_as_env_prop "${SONAR_PROJECT_BASE_DIR:-}" "sonar.projectBaseDir"
+  if [ ${#args[@]} -ne 0 ]; then
+    set -- sonar-scanner "${args[@]}" "${@:2}"
+  fi
 fi
 
 exec "$@"
