51d988004ef01eba4300e16d2b809d354daff087
Michal Duda <michal.duda@sonarsource.com>
2020-05-20 12:14:39 +0200

    SQSCANNER-77 fix permission related issues when using Docker image

--- 4/bin/entrypoint.sh
+++ 4/bin/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-declare -a args
+declare -a args=()
 
 add_env_var_as_env_prop() {
   if [ "$1" ]; then
@@ -10,27 +10,19 @@ add_env_var_as_env_prop() {
   fi
 }
 
-PROJECT_BASE_DIR="$PWD"
-if [ "${SONAR_PROJECT_BASE_DIR:-}" ]; then
-  PROJECT_BASE_DIR="${SONAR_PROJECT_BASE_DIR}"
-fi
-
-export SONAR_USER_HOME="$PROJECT_BASE_DIR/.sonar"
-
 # if nothing is passed, assume we want to run sonar-scanner
-if [ "$#" == 0 ]; then
+if [[ "$#" == 0 ]]; then
   set -- sonar-scanner
 fi
 
 # if first arg looks like a flag, assume we want to run sonar-scanner with flags
-if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
+if [[ "${1#-}" != "${1}" ]] || [[ -z "$(command -v "${1}")" ]]; then
   set -- sonar-scanner "$@"
 fi
 
 if [[ "$1" = 'sonar-scanner' ]]; then
   add_env_var_as_env_prop "${SONAR_LOGIN:-}" "sonar.login"
   add_env_var_as_env_prop "${SONAR_PASSWORD:-}" "sonar.password"
-  add_env_var_as_env_prop "${SONAR_USER_HOME:-}" "sonar.userHome"
   add_env_var_as_env_prop "${SONAR_PROJECT_BASE_DIR:-}" "sonar.projectBaseDir"
   if [ ${#args[@]} -ne 0 ]; then
     set -- sonar-scanner "${args[@]}" "${@:2}"
